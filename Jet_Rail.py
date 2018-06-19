566b97e3ca0d22a0ca3ac6596996b7aeb4bf125a
Syed Rafay <rafay_syed1@yahoo.com>
2018-06-19 22:15:35 +0500

    Visualizing training and validation data split

--- Jet_Rail.py
+++ Jet_Rail.py
@@ -3,12 +3,32 @@ import numpy as np          # For mathematical calculations
 import matplotlib.pyplot as plt  # For plotting graphs
 from datetime import datetime    # To access datetime
 from pandas import Series        # To work on series
+from sklearn.metrics import mean_squared_error
 
 # Load Dataset
 train=pd.read_csv("data/Train_SU63ISt.csv")
 test=pd.read_csv("data/Test_0qrQsBZ.csv")
 
+train = train.drop(['ID'],axis=1)
+
 # make a copy of original dataset
 train['Datetime'] = pd.to_datetime(train.Datetime,format='%d-%m-%Y %H:%M') 
 test['Datetime'] = pd.to_datetime(test.Datetime,format='%d-%m-%Y %H:%M') 
 
+valid = train.iloc[15287:18287, :]
+train = train.iloc[0:15286, :]
+
+# Visualize trainin-validation split
+train.Count.plot(figsize=(20,8), title= 'Daily Ridership', fontsize=14, label='train')
+valid.Count.plot(figsize=(20,8), title= 'Daily Ridership', fontsize=14, label='valid')
+plt.xlabel("Datetime")
+plt.ylabel("Passenger count")
+plt.legend(loc='best')
+plt.show()
+
+
+train_X = train.iloc[:, 0]
+train_y = train.iloc[:, 1]
+
+valid_X = valid.iloc[:, 0]
+valid_y = valid.iloc[:, 1]
